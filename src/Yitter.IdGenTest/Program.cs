acc40c7969a4af6bcd16cb9ca1ac973a80a36a52
zhouzj <zhouzj@zhouzj.com>
2021-03-12 15:12:21 +0800

    修改测试配置

--- src/Yitter.IdGenTest/Program.cs
+++ src/Yitter.IdGenTest/Program.cs
@@ -9,7 +9,7 @@ namespace Yitter.OrgSystem.TestA
     class Program
     {
 
-        // 测试参数
+        // 测试参数（默认配置下，最佳性能是10W/s）
         static int genIdCount = 50000;  // 计算ID数量（如果要验证50W效率，请将TopOverCostCount设置为20000或适当增加SeqBitLength）
         static short method = 1; // 1-漂移算法，2-传统算法
 
