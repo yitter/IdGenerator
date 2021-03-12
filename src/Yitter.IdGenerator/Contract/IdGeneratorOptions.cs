7cd5a0e8fc5380a638c090334cd245277dab4d06
zhouzj <zhouzj@zhouzj.com>
2021-03-13 01:46:27 +0800

    editscope

--- src/Yitter.IdGenerator/Contract/IdGeneratorOptions.cs
+++ src/Yitter.IdGenerator/Contract/IdGeneratorOptions.cs
@@ -19,51 +19,51 @@ namespace Yitter.IdGenerator
         /// 雪花计算方法
         /// （1-漂移算法|2-传统算法），默认1
         /// </summary>
-        public short Method { get; set; } = 1;
+        public virtual short Method { get; set; } = 1;
 
         /// <summary>
         /// 开始时间（UTC格式）
         /// 不能超过当前系统时间
         /// </summary>
-        public DateTime StartTime { get; set; } = DateTime.MinValue;
+        public virtual DateTime StartTime { get; set; } = DateTime.MinValue;
 
         /// <summary>
         /// 机器码
         /// 与 WorkerIdBitLength 有关系
         /// </summary>
-        public ushort WorkerId { get; set; } = 0;
+        public virtual ushort WorkerId { get; set; } = 0;
 
         /// <summary>
         /// 机器码位长
         /// 范围：2-21（要求：序列数位长+机器码位长不超过22）。
         /// 建议范围：6-12。
         /// </summary>
-        public byte WorkerIdBitLength { get; set; } = 6;//10;
+        public virtual byte WorkerIdBitLength { get; set; } = 6;//10;
 
         /// <summary>
         /// 序列数位长
         /// 范围：2-21（要求：序列数位长+机器码位长不超过22）。
         /// 建议范围：6-14。
         /// </summary>
-        public byte SeqBitLength { get; set; } = 6;//10;
+        public virtual byte SeqBitLength { get; set; } = 6;//10;
 
         /// <summary>
         /// 最大序列数（含）
         /// （由SeqBitLength计算的最大值）
         /// </summary>
-        public int MaxSeqNumber { get; set; } = 0;
+        public virtual int MaxSeqNumber { get; set; } = 0;
 
         /// <summary>
         /// 最小序列数（含）
         /// 默认11，不小于5，不大于MaxSeqNumber-2
         /// </summary>
-        public ushort MinSeqNumber { get; set; } = 11;
+        public virtual ushort MinSeqNumber { get; set; } = 11;
 
         /// <summary>
         /// 最大漂移次数（含），
         /// 默认2000，推荐范围500-10000（与计算能力有关）
         /// </summary>
-        public int TopOverCostCount { get; set; } = 2000;
+        public virtual int TopOverCostCount { get; set; } = 2000;
 
 
         public IdGeneratorOptions()
