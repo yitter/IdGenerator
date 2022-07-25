0b5f901097e061f50d561c1f0264f4cc47eec4b7
lifushen <786699380@qq.com>
2022-07-25 14:22:16 +0800

    增加redis哨兵和集群模式的支持

--- Go/source/main.go
+++ Go/source/main.go
@@ -19,27 +19,49 @@ func NextId() int64 {
 	return idgen.NextId()
 }
 
-// 注册一个 WorkerId，会先注销所有本机已注册的记录
+// RegisterOne 注册一个 WorkerId，会先注销所有本机已注册的记录
+// address: Redis连接地址，单机模式示例：127.0.0.1:6379，哨兵/集群模式示例：127.0.0.1:26380,127.0.0.1:26381,127.0.0.1:26382
+// password: Redis连接密码
+// db: Redis指定存储库，示例：1
+// sentinelMasterName: Redis 哨兵模式下的服务名称，示例：mymaster
+// maxWorkerId: WorkerId 最大值，示例：63
 //export RegisterOne
-func RegisterOne(ip *C.char, port int32, password *C.char, maxWorkerId int32) int32 {
-	return regworkerid.RegisterOne(C.GoString(ip), port, C.GoString(password), maxWorkerId)
+func RegisterOne(address *C.char, password *C.char, db int, sentinelMasterName *C.char, maxWorkerId int32) int32 {
+	return regworkerid.RegisterOne(regworkerid.RegisterConf{
+		Address:     C.GoString(address),
+		Password:    C.GoString(password),
+		DB:          db,
+		MasterName:  C.GoString(sentinelMasterName),
+		MaxWorkerId: maxWorkerId,
+	})
 }
 
-// 注册多个 WorkerId，会先注销所有本机已注册的记录
-///export RegisterMany
-func RegisterMany(ip *C.char, port int32, password *C.char, maxWorkerId int32, totalCount int32) []int32 {
-	//values := regworkerid.RegisterMany(C.GoString(ip), port, C.GoString(password), maxWorkerId, totalCount)
-	//return (*C.int)(unsafe.Pointer(&values))
-	return regworkerid.RegisterMany(C.GoString(ip), port, C.GoString(password), maxWorkerId, totalCount)
+// RegisterMany 注册多个 WorkerId，会先注销所有本机已注册的记录
+// address: Redis连接地址，单机模式示例：127.0.0.1:6379，哨兵/集群模式示例：127.0.0.1:26380,127.0.0.1:26381,127.0.0.1:26382
+// password: Redis连接密码
+// db: Redis指定存储库，示例：1
+// sentinelMasterName: Redis 哨兵模式下的服务名称，示例：mymaster
+// maxWorkerId: WorkerId 最大值，示例：63
+// totalCount: 获取N个WorkerId，示例：5
+//export RegisterMany
+func RegisterMany(address *C.char, password *C.char, db int, sentinelMasterName *C.char, maxWorkerId int32, totalCount int32) []int32 {
+	return regworkerid.RegisterMany(regworkerid.RegisterConf{
+		Address:     C.GoString(address),
+		Password:    C.GoString(password),
+		DB:          db,
+		MasterName:  C.GoString(sentinelMasterName),
+		MaxWorkerId: maxWorkerId,
+		TotalCount:  totalCount,
+	})
 }
 
-// 注销本机已注册的 WorkerId
+// UnRegister 注销本机已注册的 WorkerId
 //export UnRegister
 func UnRegister() {
 	regworkerid.UnRegister()
 }
 
-// 检查本地WorkerId是否有效（0-有效，其它-无效）
+// Validate 检查本地WorkerId是否有效（0-有效，其它-无效）
 //export Validate
 func Validate(workerId int32) int32 {
 	return regworkerid.Validate(workerId)
@@ -70,13 +92,37 @@ func main() {
 			time.Sleep(time.Duration(1000) * time.Millisecond)
 		}
 	} else {
-		workerIdList := regworkerid.RegisterMany("localhost", 6379, "", 4, 3)
-		for _, value := range workerIdList {
-			fmt.Println("注册的WorkerId:", value)
-		}
 
-		//var workerId = regworkerid.RegisterOne("localhost", 6379, "", 4)
-		//fmt.Println("注册的WorkerId:", workerId)
+		//// Redis单机模式下，获取多个WorkerId
+		//workerIdList := regworkerid.RegisterMany(regworkerid.RegisterConf{
+		//	Address:     "127.0.0.1:6379",
+		//	Password:    "",
+		//	DB:          0,
+		//	MaxWorkerId: 4,
+		//	TotalCount:  3,
+		//})
+		//for _, value := range workerIdList {
+		//	fmt.Println("RegisterMany--注册的WorkerId:", value)
+		//}
+
+		// Redis单机模式下，获取一个WorkerId
+		var workerId = regworkerid.RegisterOne(regworkerid.RegisterConf{
+			Address:     "127.0.0.1:6379",
+			Password:    "",
+			DB:          0,
+			MaxWorkerId: 4,
+		})
+		fmt.Println("RegisterOne--注册的WorkerId:", workerId)
+
+		//// Redis哨兵模式下，获取一个WorkerId
+		//var workerId = regworkerid.RegisterOne(regworkerid.RegisterConf{
+		//	//Address:     "127.0.0.1:26380,127.0.0.1:26381,127.0.0.1:26382",
+		//	Password:    "",
+		//	DB:          0,
+		//	MasterName:  "mymaster", // 哨兵模式下的服务名称
+		//	MaxWorkerId: 4,
+		//})
+		//fmt.Println("RegisterOne--注册的WorkerId:", workerId)
 
 		fmt.Println("end")
 		time.Sleep(time.Duration(300) * time.Second)
