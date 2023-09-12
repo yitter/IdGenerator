10409330f30293fd624b4fe8fde073ad58f7d093
Albert <amuluowin@163.com>
2023-09-12 11:47:30 +0800

    修复多平台编译问题

--- PHP/src/snowflake/spinlock.h
+++ PHP/src/snowflake/spinlock.h
@@ -8,4 +8,14 @@ extern void spin_lock(atomic_t *lock, uint32_t pid);
 
 extern void spin_unlock(atomic_t *lock, uint32_t pid);
 
+#if defined(WIN32)
+#define atomic_cpu_pause() MemoryBarrier();
+#elif defined(__x86_64__)
+#define atomic_cpu_pause() __asm__ __volatile__("pause")
+#elif defined(__aarch64__)
+#define atomic_cpu_pause() __asm__ __volatile__("yield")
+#else
+#define atomic_cpu_pause()
+#endif
+
 #endif
